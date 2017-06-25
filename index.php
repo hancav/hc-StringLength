2610eac7e4570cd653e2831f56008df9b3f63ab5
AZI7-Hugo <hugo.cavalaria.aux@gmail.com>
2017-06-25 03:37:18 +0100

    Added extra function showString and test example.

--- index.php
+++ index.php
@@ -6,4 +6,10 @@ $string = 'Test String';
 
 $calclen = \hcStringLength\String::hcStrLength($string);
 
+// Class call result
 echo "\n Length of : # $string # is : # $calclen # elements. \n";
+
+$showstr = showString($string);
+
+// External function call result
+echo "\n  $showstr \n";
--- src/functions.php
+++ src/functions.php
@@ -1,2 +1,7 @@
 <?php
 /* This is file designed to hold extra funcions for the module package */
+
+function showString($string){
+  $toRet= "This is the string to analyze: #".$string."#";
+  return ($toRet);
+}
