3eb6087c71f957b99ef7e75324ef26ac2f7dee1f
AZI7-Hugo <hugo.cavalaria.aux@gmail.com>
2017-06-25 02:35:49 +0100

    Main class for the package .

---
+++ src/String.php
@@ -0,0 +1,12 @@
+<?php
+/* This is a simple example class to test: composer / packages / packagist / github / sourcetree  */
+
+namespace hcStringLength;
+
+class String {
+
+  public static function hcStrLength($string) {
+    return strlen($string);
+  }
+
+}
